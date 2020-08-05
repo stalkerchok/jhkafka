2b6283519b74e517ae709849794213828b9192d5
arubashenkov <arubashenkov@reunico.com>
2020-08-06 01:16:11 +0300

    Initial version of jhkafka generated by JHipster-6.9.1

---
+++ src/main/java/com/reunico/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.reunico.repository;
+
+import com.reunico.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/reunico/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.reunico.service.dto;
